7f6888044b54dce4787536d43b0365f1cf5dd2f4
Alexander Larsson <alexl@redhat.com>
2014-05-28 08:50:09 +0200

    Add --grant-secret hostconfig option
    If you run something like
      docker run --grant-secret foo image
    
    Then the "foo" secret will be copied into the /run/secrets tmpfs in the
    container. Additionally, all host-based secrets will be copied into
    all containers running on a specific host.
    
    Docker-DCO-1.1-Signed-off-by: Alexander Larsson <alexl@redhat.com> (github: alexlarsson)

--- daemon/execdriver/driver.go
+++ daemon/execdriver/driver.go
@@ -138,6 +138,7 @@ type Command struct {
 	Mounts             []Mount             `json:"mounts"`
 	AllowedDevices     []*devices.Device   `json:"allowed_devices"`
 	AutoCreatedDevices []*devices.Device   `json:"autocreated_devices"`
+	CreateFiles        map[string][]byte   `json:"create_files"`
 
 	Terminal     Terminal `json:"-"`             // standard or tty terminal
 	Console      string   `json:"-"`             // dev/console path
